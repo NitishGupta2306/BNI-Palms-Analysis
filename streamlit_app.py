067bb4c9eda536add173a8d291c161c65d041f87
Nitish <nitishgupta2306@gmail.com>
2025-03-19 17:23:06 +0400

    navigation menu set up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.title("BNI Palms Analysis")
+# Page setup:
+intro_page = st.Page(
+    page = "views/introduction_page.py",
+    title = "Introduction",
+    icon = ":material/waving_hand:",
+    default = True,
+)
+
+report_page = st.Page(
+    page = "views/reports_page.py",
+    title = "Palms Analysis",
+    icon = ":material/partner_exchange:",
+)
+
+# Navigation setup:
+pg = st.navigation(
+    {
+        "Info": [intro_page],
+        "Reports": [report_page],
+    }
+)
+pg.run()
